785af560d36f18928e0734a881b0c28a5244b8f3
Tomas Cuevas <memo_mister398@hotmail.com>
2023-01-07 12:22:39 -0300

    Tipos de datos - continuación

--- 01-typescript-intro/src/bases/01-types.ts
+++ 01-typescript-intro/src/bases/01-types.ts
@@ -1,3 +1,9 @@
-export const name = "Tomas";
+export let name: string = "Tomas";
 export const age = 22;
 export const isValid: boolean = false;
+
+name = "Sorana";
+
+export const templateString = `Hello, soy un TemplateString, suma 2 + 5 por favor: ${
+  2 + 5
+}, y ahora colona la variable name: ${name}`;
