9085465e6b5021feaa5e0e7e5528b4daaf19bde8
Tomas Cuevas <memo_mister398@hotmail.com>
2023-01-07 14:08:31 -0300

    Clases y forma abreviada

---
+++ 01-typescript-intro/src/bases/03-classes.ts
@@ -0,0 +1,13 @@
+export class Pokemon {
+  // public id: number;
+  // public name: string;
+
+  // constructor(id: number, name: string) {
+  //   this.id = id;
+  //   this.name = name;
+  // }
+
+  constructor(public readonly id: number, public name: string) {}
+}
+
+export const charmander = new Pokemon(1, "Charmander");
--- 01-typescript-intro/src/main.ts
+++ 01-typescript-intro/src/main.ts
@@ -1,11 +1,12 @@
 // import { name, age } from "./bases/01-types";
-import { pokemon, pokemons } from "./bases/02-objects";
+// import { pokemon, pokemons } from "./bases/02-objects";
+import { charmander } from "./bases/03-classes";
 
 import "./style.css";
 
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 app.innerHTML = `
-  <h1>Hello ${JSON.stringify(pokemons)} !</h1>
+  <h1>Hello ${charmander.name} !</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `;
