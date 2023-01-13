9547f6fb749e721df746b75c7ce57edb6fecfb3f
Tomas Cuevas <memo_mister398@hotmail.com>
2023-01-13 17:23:24 -0300

    Inyectar servicios en otros servicios

--- 02-car-dealership/src/seed/seed.module.ts
+++ 02-car-dealership/src/seed/seed.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
-import { SeedService } from './seed.service';
+
+//* modules *//
+import { CarsModule } from 'src/cars/cars.module';
+import { BrandsModule } from '../brands/brands.module';
+
+//* controllers *//
 import { SeedController } from './seed.controller';
+
+//* services *//
+import { SeedService } from './seed.service';
 
 @Module({
   controllers: [SeedController],
-  providers: [SeedService]
+  providers: [SeedService],
+  imports: [CarsModule, BrandsModule],
 })
 export class SeedModule {}
--- 02-car-dealership/src/seed/seed.service.ts
+++ 02-car-dealership/src/seed/seed.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@nestjs/common';
 
+//* seed data *//
+import { CARS_SEED } from './data/cars.seed';
+import { BRANDS_SEED } from './data/brands.seed';
+
+//* services *//
+import { CarsService } from 'src/cars/cars.service';
+import { BrandsService } from 'src/brands/brands.service';
+
 @Injectable()
 export class SeedService {
+  constructor(
+    private readonly carsService: CarsService,
+    private readonly brandsService: BrandsService,
+  ) {}
+
   public populateDB() {
+    this.carsService.fillCarsWithSeedData(CARS_SEED);
+    this.brandsService.fillBrandsWithSeed(BRANDS_SEED);
+
     return 'SEED executed';
   }
 }
