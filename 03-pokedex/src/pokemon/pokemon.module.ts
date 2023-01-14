5d5bd529c60fce389359d6e61057a292d233c584
Tomas Cuevas <memo_mister398@hotmail.com>
2023-01-14 15:57:05 -0300

    Crear esquemas y Modelos

--- 03-pokedex/src/pokemon/pokemon.module.ts
+++ 03-pokedex/src/pokemon/pokemon.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+//* services *//
 import { PokemonService } from './pokemon.service';
+
+//* controllers *//
 import { PokemonController } from './pokemon.controller';
+
+//* enties *//
+import { Pokemon, PokemonSchema } from './entities/pokemon.entity';
 
 @Module({
   controllers: [PokemonController],
-  providers: [PokemonService]
+  providers: [PokemonService],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Pokemon.name,
+        schema: PokemonSchema,
+      },
+    ]),
+  ],
 })
 export class PokemonModule {}
