c9d34304f1ffe69bdd74fb1b47e02936e22d607e
Tomas Cuevas <memo_mister398@hotmail.com>
2023-01-14 13:37:33 -0300

    Global Prefix

--- 03-pokedex/src/app.module.ts
+++ 03-pokedex/src/app.module.ts
@@ -2,11 +2,15 @@ import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 
+//* models *//
+import { PokemonModule } from './pokemon/pokemon.module';
+
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
+    PokemonModule,
   ],
 })
 export class AppModule {}
