72fd140d06e9b29d24418633260b3e1c5b2dd71d
Tomas Cuevas <memo_mister398@hotmail.com>
2023-01-16 10:21:48 -0300

    POST - Recibir y validar la data

--- 03-pokedex/src/main.ts
+++ 03-pokedex/src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+
+//* module *//
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api/v2');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
 
   await app.listen(3000);
 }
--- 03-pokedex/src/pokemon/dto/create-pokemon.dto.ts
+++ 03-pokedex/src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,12 @@
-export class CreatePokemonDto {}
+import { IsString, IsInt, IsPositive, Min, MinLength } from 'class-validator';
+
+export class CreatePokemonDto {
+  @IsString()
+  @MinLength(1)
+  readonly name: string;
+
+  @IsInt()
+  @IsPositive()
+  @Min(1)
+  readonly no: number;
+}
