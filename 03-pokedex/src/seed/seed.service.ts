53c72ffce8041ed05747201fde3190aadcf7cfc2
Tomas Cuevas <memo_mister398@hotmail.com>
2023-01-17 11:40:33 -0300

    Realizar petición http desde Nest

--- 03-pokedex/src/seed/seed.service.ts
+++ 03-pokedex/src/seed/seed.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+
+//* interfaces *//
+import { IPokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
-  executeSeed() {
-    return 'Seed executed';
+  private readonly axios: AxiosInstance = axios;
+
+  async executeSeed() {
+    const { data } = await this.axios.get<IPokeResponse>(
+      'https://pokeapi.co/api/v2/pokemon?limit=10',
+    );
+
+    const pokemons = data.results.map(({ name, url }) => ({
+      name: name,
+      id: +url.split('/').at(-2),
+    }));
+
+    return pokemons;
   }
 }
