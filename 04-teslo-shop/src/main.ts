d5103ca887aa174cf7ae270a4101fcbab9f5d970
Tomas Cuevas <memo_mister398@hotmail.com>
2023-01-20 15:52:10 -0300

    Create Product DTO

--- 04-teslo-shop/src/main.ts
+++ 04-teslo-shop/src/main.ts
@@ -1,8 +1,20 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+
+//* modules *//
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  app.setGlobalPrefix('/api');
+
   await app.listen(3000);
 }
 bootstrap();
