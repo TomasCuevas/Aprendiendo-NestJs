8c7e1aa1367b566dc285a69bd6833eff3eaa2fa6
Tomas Cuevas <memo_mister398@hotmail.com>
2023-01-22 16:36:46 -0300

    Update en TypeORM

--- 04-teslo-shop/src/products/products.service.ts
+++ 04-teslo-shop/src/products/products.service.ts
@@ -67,14 +67,28 @@ export class ProductsService {
     return product;
   }
 
-  async update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async update(id: string, updateProductDto: UpdateProductDto) {
+    const product = await this.productRepository.preload({
+      id,
+      ...updateProductDto,
+    });
+
+    if (!product) {
+      throw new NotFoundException(`Product with id "${id}" not found.`);
+    }
+
+    try {
+      await this.productRepository.save(product);
+
+      return product;
+    } catch (error) {
+      this.handleDBExceptions(error);
+    }
   }
 
   async remove(id: string) {
     const product = await this.findOne(id);
-
-    await this.productRepository.delete({ id });
+    await this.productRepository.remove(product);
   }
 
   private handleDBExceptions(error: any) {
