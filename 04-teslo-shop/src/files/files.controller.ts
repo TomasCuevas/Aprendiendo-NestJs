ed53854e4015fbf29aafe248033aaf26a2a57304
Tomas Cuevas <memo_mister398@hotmail.com>
2023-01-25 16:32:55 -0300

    Subir un archivo al backend

---
+++ 04-teslo-shop/src/files/files.controller.ts
@@ -0,0 +1,21 @@
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+
+//* services *//
+import { FilesService } from './files.service';
+
+@Controller('files')
+export class FilesController {
+  constructor(private readonly filesService: FilesService) {}
+
+  @Post()
+  @UseInterceptors(FileInterceptor('file'))
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    return file;
+  }
+}
