5b8c9eacf774c7f9046bb977ab4005139901053b
Tomas Cuevas <memo_mister398@hotmail.com>
2023-01-25 16:02:56 -0300

    Renombrar tablas

--- 04-teslo-shop/src/products/entities/product-image.entity.ts
+++ 04-teslo-shop/src/products/entities/product-image.entity.ts
@@ -3,7 +3,7 @@ import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 //* entities *//
 import { Product } from './product.entity';
 
-@Entity()
+@Entity({ name: 'product_images' })
 export class ProductImage {
   @PrimaryGeneratedColumn()
   id: number;
