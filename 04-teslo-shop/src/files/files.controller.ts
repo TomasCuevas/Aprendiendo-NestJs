289249be65a63d37198aba1921c78d4059f6ee35
Tomas Cuevas <memo_mister398@hotmail.com>
2023-01-26 15:34:45 -0300

    Servir archivos de manera controlada

--- 04-teslo-shop/src/files/files.controller.ts
+++ 04-teslo-shop/src/files/files.controller.ts
@@ -1,12 +1,16 @@
 import {
   BadRequestException,
   Controller,
+  Get,
+  Param,
   Post,
+  Res,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
+import { Response } from 'express';
 
 //* helpers *//
 import { fileFilter, fileNamer } from './helpers';
@@ -18,11 +22,21 @@ import { FilesService } from './files.service';
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
 
+  @Get('product/:imageName')
+  findProductImage(
+    @Param('imageName') imageName: string,
+    @Res() res: Response,
+  ) {
+    const path = this.filesService.getStaticProductImage(imageName);
+
+    res.sendFile(path);
+  }
+
   @Post()
   @UseInterceptors(
     FileInterceptor('file', {
       fileFilter: fileFilter,
-      // limits: { fieldSize: 1024 },
+      limits: { fieldSize: 1024 },
       storage: diskStorage({
         destination: './static/products',
         filename: fileNamer,
@@ -34,6 +48,8 @@ export class FilesController {
       throw new BadRequestException('Make sure that the file is an image');
     }
 
-    return;
+    const secure_url = file.filename;
+
+    return secure_url;
   }
 }
