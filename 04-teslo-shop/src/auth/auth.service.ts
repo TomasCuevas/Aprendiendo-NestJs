d1456445804e511afe8731ea64773cfc1a7e470f
Tomas Cuevas <memo_mister398@hotmail.com>
2023-01-27 15:36:20 -0300

    Encriptar la contraseña

--- 04-teslo-shop/src/auth/auth.service.ts
+++ 04-teslo-shop/src/auth/auth.service.ts
@@ -5,6 +5,7 @@ import {
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 
 //* dtos *//
 import { CreateUserDto } from './dto';
@@ -20,8 +21,15 @@ export class AuthService {
 
   async create(createUserDto: CreateUserDto) {
     try {
-      const user = this.userRepository.create(createUserDto);
+      const { password, ...userData } = createUserDto;
+
+      const user = this.userRepository.create({
+        ...userData,
+        password: bcrypt.hashSync(password, bcrypt.genSaltSync()),
+      });
       await this.userRepository.save(user);
+
+      delete user.password;
 
       return user;
     } catch (error) {
