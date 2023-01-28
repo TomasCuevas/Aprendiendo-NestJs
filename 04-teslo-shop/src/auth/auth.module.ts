5242a18b3eb5fda3e17a9a5748f64c8f2826a959
Tomas Cuevas <memo_mister398@hotmail.com>
2023-01-27 21:29:23 -0300

    Nest Authentication - Passport

--- 04-teslo-shop/src/auth/auth.module.ts
+++ 04-teslo-shop/src/auth/auth.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 //* controllers *//
@@ -13,6 +15,16 @@ import { User } from './entities';
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+
+    // JwtModule.register({
+    //   secret: '123123123',
+    //   signOptions: {
+    //     expiresIn: '2h',
+    //   },
+    // }),
+  ],
 })
 export class AuthModule {}
