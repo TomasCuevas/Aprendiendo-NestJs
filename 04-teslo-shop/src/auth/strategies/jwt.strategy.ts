f07458accba9c6e19d81b511f47b0dda45a78a23
Tomas Cuevas <memo_mister398@hotmail.com>
2023-01-29 11:10:45 -0300

    Tarea: Cambiar email por id en el Payload

--- 04-teslo-shop/src/auth/strategies/jwt.strategy.ts
+++ 04-teslo-shop/src/auth/strategies/jwt.strategy.ts
@@ -24,15 +24,13 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: IJwtPayload): Promise<User> {
-    const { email } = payload;
+    const { id } = payload;
 
-    const user = await this.userRepository.findOneBy({ email });
+    const user = await this.userRepository.findOneBy({ id });
 
     if (!user) {
       throw new UnauthorizedException(`Token not valid.`);
     }
-
-    console.log(user);
 
     if (!user.isActive) {
       throw new UnauthorizedException(`User is inactive, talk with an admin.`);
