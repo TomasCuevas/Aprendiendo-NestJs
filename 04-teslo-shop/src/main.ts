3ce1ca26849582a2981aea722bff475c6378bbf2
Tomas Cuevas <memo_mister398@hotmail.com>
2023-01-31 11:54:33 -0300

    Nestjs swagger - OpenAPI Specification

--- 04-teslo-shop/src/main.ts
+++ 04-teslo-shop/src/main.ts
@@ -1,5 +1,6 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 //* modules *//
 import { AppModule } from './app.module';
@@ -15,6 +16,14 @@ async function bootstrap() {
     }),
   );
   app.setGlobalPrefix('/api');
+
+  const config = new DocumentBuilder()
+    .setTitle('Teslo RESTFul API')
+    .setDescription('Teslo shop endpoints')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT);
   logger.log(`App running on port ${process.env.PORT}`);
