aee339933c19fd3e200f69af751dd24318e01327
Tomas Cuevas <memo_mister398@hotmail.com>
2023-02-02 14:18:34 -0300

    Cliente - Vite Vanilla TypeScript

---
+++ 04-ws-client/src/main.ts
@@ -0,0 +1,12 @@
+import "./style.css";
+
+import { connectToServer } from "./socket-client";
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <div>
+  <h1>Websocket client</h1>
+  <span>Offline</span>
+  </div>
+`;
+
+connectToServer();
