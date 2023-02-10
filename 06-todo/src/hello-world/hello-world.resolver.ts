69ef951b713d7e22cc367d1503b93402467198dd
Tomas Cuevas <memo_mister398@hotmail.com>
2023-02-10 13:45:57 -0300

    Generar un nuevo query

--- 06-todo/src/hello-world/hello-world.resolver.ts
+++ 06-todo/src/hello-world/hello-world.resolver.ts
@@ -1,9 +1,17 @@
-import { Resolver, Query } from '@nestjs/graphql';
+import { Resolver, Query, Float } from '@nestjs/graphql';
 
 @Resolver()
 export class HelloWorldResolver {
-  @Query(() => String)
+  @Query(() => String, {
+    name: 'hello',
+    description: '"Hello World" is what returns',
+  })
   helloWorld(): string {
     return 'Hello World!';
+  }
+
+  @Query(() => Float, { name: 'randomNumber' })
+  getRandomNumber(): number {
+    return Math.random() * 100;
   }
 }
